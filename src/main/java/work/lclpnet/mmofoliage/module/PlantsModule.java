ef71583678961d6217e3abedf9408b3076e6ffbb
LCLP <lukas@lclpnet.work>
2022-02-02 12:21:31 +0100

    added translations

--- src/main/java/work/lclpnet/mmofoliage/module/PlantsModule.java
+++ src/main/java/work/lclpnet/mmofoliage/module/PlantsModule.java
@@ -1,8 +1,19 @@
 package work.lclpnet.mmofoliage.module;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+import work.lclpnet.mmocontent.block.MMOBlockRegistrar;
+import work.lclpnet.mmofoliage.MMOFoliage;
+
 public class PlantsModule implements IModule {
 
     @Override
     public void register() {
+        new MMOBlockRegistrar(AbstractBlock.Settings.of(Material.REPLACEABLE_PLANT)
+                .noCollision()
+                .breakInstantly()
+                .sounds(BlockSoundGroup.GRASS))
+                .register(MMOFoliage.identifier("bush"));
     }
 }
